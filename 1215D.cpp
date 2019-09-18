b7a8c857ce0cd461a18040f0801a8188fc7e3bdd
Elie Maamary <elie.maamary@murex.com>
2019-09-18 12:47:35 +0300

    add problems

---
+++ 1215D.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+typedef vector<vector<int>> vvi;
+
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int MX = 100001; //check the limits, dummy
+
+int main() {
+    int n; cin >> n;
+    int x = 0, y = 0, sx = 0, sy = 0;
+    for(int i = 0; i < n; i++) {
+        char in; cin >> in;
+        if(in == '?')
+            if(i < n/2) sx++;
+            else sy++;
+        else 
+            if(i < n/2) x += (in - '0');
+            else y += (in - '0');
+    }
+
+    if(x + 9*(sx/2 + sx%2) - y - 9*(sy/2 + sy%2) != 0) cout << "Monocarp" << endl;
+    else cout << "Bicarp" << endl;
+
+    return 0;
+}
