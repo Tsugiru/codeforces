1d33421b4234a32720d9f0221cc97dc00c24eb81
Elie Maamary <elie.maamary@murex.com>
2019-09-23 09:10:13 +0300

    add problems

---
+++ 1221D.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int MX = 100001; //check the limits, dummy
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int q; cin >> q;
+    while(q--) {
+        int n; cin >> n;
+        vpi v(n, {0, 0});
+        F0R(i, n) {
+            cin >> v[i].first;
+            cin >> v[i].second;
+        }
+
+        vvl dp(n + 1, vl(3, INF));
+        dp[0][0] = dp[0][1] = dp[0][2] = 0;
+        FOR(i, 1, n + 1) {
+            F0R(j, 3) {
+                F0R(k, 3) {
+                    if(i == 1 || v[i - 1].first + j != v[i - 2].first + k) {
+                        dp[i][j] = min(dp[i][j], j*v[i - 1].second + dp[i - 1][k]);
+                    }
+                }
+            }
+        }
+        cout << min(dp[n][0], min(dp[n][1], dp[n][2])) << endl;
+    }
+}
