2f7c438b1353ba934c27d48cd102d7b6c73eb1aa
Elie Maamary <elie.maamary@murex.com>
2019-09-25 18:42:04 +0300

    add problems

---
+++ 1230A.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pdd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<cd> vcd;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int inf = 1e0;
+const int MX = 100001; //check the limits, dummy
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    for(int i = 0; i < 16; i++) {
+        int sum1 = 0, sum2 = 0;
+        sum1 += (i & 1 ? a : 0) + (i & (1 << 1) ? b : 0) + (i & (1 << 2) ? c : 0) + (i & (1 << 3) ? d : 0);
+        sum2 += (i & 1 ? 0 : a) + (i & (1 << 1) ? 0 : b) + (i & (1 << 2) ? 0 : c) + (i & (1 << 3) ? 0 : d);
+        if (sum1 == sum2) {
+            cout << "YES" << endl;
+            return 0;
+        }
+    }
+
+    cout << "NO" << endl;
+}
---
+++ 1230B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<ld,ld> pdd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+typedef vector<cd> vcd;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<ll>> vvl;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MOD = 1000000007;
+const ll INF = 1e18;
+const int inf = 1e0;
+const int MX = 100001; //check the limits, dummy
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int n, k; cin >> n >> k;
+    string s; cin >> s;
+    
+    for(int i = 0; i < n && k > 0; i++) {
+        char cur = n > 1 && i == 0 ? '1' : '0';
+        if(s[i] != cur)
+            s[i] = cur, k--;
+    }
+
+    cout << s << endl;
+}
